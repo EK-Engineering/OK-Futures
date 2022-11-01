b6a68f05ba81891fd09971d02eafaee8c32bb7c4
Katie Schrattenholzer <ktschr11@gmail.com>
2022-11-01 10:33:13 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  //the pages you want tailwind to work with. Any File types that you want tailwind in
+  //added flowbite into the content tag
+  content: [
+    "./src/**/*{.jsx,.js,}",
+    "./node_modules/flowbite/**/*{.jsx,.js,}"
+  ],
+  //the variables for tailwind. you can change or extend(which creates new ones)
+  theme: {
+    extend: {
+      "cadet-blue": "rgba(97, 150, 153, 0.705)"
+    
+    },
+  },
+  //tailwind specific, stuff only gets added here if the docs say so
+  plugins: [
+    require('flowbite/plugin')
+]
+}
