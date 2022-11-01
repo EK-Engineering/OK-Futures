04ff5865a482e91f976010c9ba672663ab9ccf27
EmilyDSarani <emily.sarani@gmail.com>
2022-11-01 18:28:05 -0500

    took out the hello world

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -5,9 +5,6 @@ import Foot from '../../Components/Layout/Foot';
 export default function Home() {
   return (
     <div>
-    <h1 className='text-emerald-500 text-3xl text-center pb-3 font-mono'>
-        Hello World!
-    </h1>
    <header><Header/></header> 
    <footer><Foot /></footer>
     <script src="../path/to/flowbite/dist/flowbite.js"></script>
