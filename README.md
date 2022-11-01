60be4a582417abd67ab51518af6aa2de28c7a657
k-schrattenholzer <ktschr11@gmail.com>
2022-11-01 11:38:30 -0700

    Adds horizontal layout to key truths

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Read This
+
+//
+import 'flowbite';
+import KeyTruths from '../.././Components/KeyTruths/KeyTruths.jsx';
+
+
+export default function Home() {
+  return (
+    <>
+    <KeyTruths />
+    <script src="../path/to/flowbite/dist/flowbite.js"></script>
+    </>
+  )
+}
--- src/Components/KeyTruths/KeyTruths.jsx
+++ src/Components/KeyTruths/KeyTruths.jsx
@@ -1,21 +1,21 @@
 import Card from '../Card/Card.jsx';
 
 const content = [
-    {num: 1, txt: 'truth 1'},
-    {num: 2, txt: 'truth 2'},
-    {num: 3, txt: 'truth 3'},
-    {num: 4, txt: 'truth 4'}     
+    {num: 1, txt: 'Help individuals make well-informed choices in education, training and careers'},
+    {num: 2, txt: 'Develop an integrated high-quality system of education and training that responds to constantly evolving needs'},
+    {num: 3, txt: 'Promote employer recognition and career development based on skills and mastery'},
+    {num: 4, txt: 'Foster a culture that supports and celebrates lifelong learning'}     
     ]
 
 export default function KeyTruths() {
   return (
-    <> 
+    <div class="flex"> 
     {
       content.map(
         (truth) => 
           <Card num={truth.num} txt={truth.txt} />
         )
     }
-    </>
+    </div>
   )
 }
