9969f78a1e01f08b36837c70c2f49f82e100fe19
k-schrattenholzer <ktschr11@gmail.com>
2022-11-03 11:15:30 -0700

    sets color background for truths

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,25 @@
-export default function Card({ num, txt }) {
+// because tailwind doesn't support dynamic segments within classnames
+// we're storing the full classnames for each color in a variable which we'll call on, dependent on the prop passed to the card
+
+const greenStyle = "flex flex-row p-6 max-w-sm bg-green-l rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+const blueStyle = "flex flex-row p-6 max-w-sm bg-blue-d rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+const yellowStyle = "flex flex-row p-6 max-w-sm bg-yellow-d rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+const rustStyle = "flex flex-row p-6 max-w-sm bg-rust-d rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+
+export default function Card({ color, txt }) {
+
+    function getBgColor() {
+        if (color === 'blue') return blueStyle;
+        if (color === 'green') return greenStyle;
+        if (color === 'rust') return rustStyle;
+        if (color === 'yellow') return yellowStyle;
+    }
 
   return (
-    <div  
-        className="flex flex-col justify-center p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <h5 
-            className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {num}</h5>
-        <p
-            className="font-normal text-center text-gray-700 dark:text-gray-400">
-                {txt}</p>
+    <div className={getBgColor()} >
+        <p className="font-normal text-white lg:text-gray-700 dark:text-gray-400">
+            {txt}
+        </p>
     </div>
         )
     }
--- src/Components/KeyTruths/KeyTruths.jsx
+++ src/Components/KeyTruths/KeyTruths.jsx
@@ -1,19 +1,20 @@
 import Card from '../Card/Card.jsx';
+import uuid from 'react-uuid';
 
 const content = [
-    {num: 1, txt: 'Provide assistance in making informed decisions on education, training and careers.'},
-    {num: 2, txt: 'Develop an integrated high-quality system of education and training that is responsive to the evolving needs of our community.'},
-    {num: 3, txt: 'Encourage employer recognition and professional development based on skills and knowledge.'},
-    {num: 4, txt: 'Foster an environment that supports and rewards lifelong learning.'}     
+    {color: 'blue', txt: 'Provide assistance in making informed decisions on education, training and careers.'},
+    {color: 'green', txt: 'Develop an integrated high-quality system of education and training that is responsive to the evolving needs of our community.'},
+    {color: 'yellow', txt: 'Encourage employer recognition and professional development based on skills and knowledge.'},
+    {color: 'rust', txt: 'Foster an environment that supports and rewards lifelong learning.'}     
     ]
 
 export default function KeyTruths() {
   return (
-    <div className="flex flex-row"> 
+    <div className="flex flex-col"> 
     {
       content.map(
         (truth) => 
-          <Card key={truth.num + 1} num={truth.num} txt={truth.txt} />
+          <Card key={uuid()} color={truth.color} txt={truth.txt} />
         )
     }
     </div>
