2fdbbaabff4aca6461bf19725588b24f4b42d677
k-schrattenholzer <ktschr11@gmail.com>
2022-11-07 13:25:53 -0800

    Adds all components to landing page, carousels aren't displaying yet

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,10 +1,18 @@
 import VideoPlayer from '../.././Components/Video/VideoPlayer.jsx';
 import stockvid from '../.././Assets/stockvid.mp4';
+import KeyTruths from '../.././Components/KeyTruths/KeyTruths.jsx';
+import Carousels from '../.././Components/Carousel/Carousel.jsx';
+import Header from '../../Components/Layout/Header.jsx';
+import Foot from '../../Components/Layout/Foot.jsx';
 
 export default function Home() {
   return (
-    <div className="w-96 p-4">
-    <VideoPlayer content={stockvid}/>
+    <div className="w-screen p-4 flex flex-col content-center">
+      <Header />
+      <VideoPlayer content={stockvid} />
+      <KeyTruths />
+      <Carousels />
+      <Foot />
     </div>
   )
 }
