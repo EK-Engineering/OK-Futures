2e7ef34043b507141ea70d8726af1b7c4eec7a4b
k-schrattenholzer <ktschr11@gmail.com>
2022-11-13 09:27:53 -0800

    replaces buggy flowbite-react footer with tailwind footer

--- src/Components/Layout/Foot.jsx
+++ src/Components/Layout/Foot.jsx
@@ -3,17 +3,23 @@ import { Footer } from 'flowbite-react';
 
 export default function Foot() {
   return (
-    <Footer container={true}>
-      <Footer.Copyright
-        href="/"
-        by="Oklahoma Futures™"
-        year={2022}
-      />
-      <Footer.LinkGroup>
-        {/* <Footer.Link href="#">
-      About
-    </Footer.Link> */}
-      </Footer.LinkGroup>
-    </Footer>
+    <footer class="p-4 bg-transparent w-full p-3 border-none dark:bg-gray-800">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+      </span>
+      {/* <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">Contact</a>
+        </li>
+      </ul> */}
+    </footer>
   )
 }
