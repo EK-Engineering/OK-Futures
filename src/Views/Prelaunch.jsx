e66c757d5a5318545b816aceb41d71388c55cc64
k-schrattenholzer <ktschr11@gmail.com>
2023-02-24 15:22:25 -0800

    text and image centered

--- src/Views/Prelaunch.jsx
+++ src/Views/Prelaunch.jsx
@@ -1,15 +1,20 @@
 
 export default function Prelaunch() {
     return (
-        <div 
-            className="
-                    mx-auto
-                    h-screen
-                    w-screen
-                    bg-prelaunch
-                    ">
-                        <p>this is a div</p>
-
+        <div className="max-w-screen-lg ">
+            <div 
+                className="
+                        mx-auto
+                        h-screen
+                        w-screen
+                        bg-prelaunch
+                        bg-no-repeat
+                        bg-clip-border
+                        bg-center
+                        ">
+                <p className="text-center grid h-screen place-items-center">this is a div</p>
+    
+            </div>
         </div>
     )
 }
