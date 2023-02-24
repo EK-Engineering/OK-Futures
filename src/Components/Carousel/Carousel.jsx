fef4bfaf7cd2a9df4838e75cc15884cb8b658c4c
k-schrattenholzer <ktschr11@gmail.com>
2023-02-24 13:51:53 -0800

    update text

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -11,7 +11,7 @@ export default function Carousel() {
         <div className="max-w-sm m-5">
           <Card
             imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg"
+            imgSrc="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
           >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               SKILLS DEMAND FOR THE FUTURE ECONOMY
@@ -24,10 +24,10 @@ export default function Carousel() {
         <div className="max-w-sm m-5">
           <Card
             imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg"
+            imgSrc="https://images.unsplash.com/photo-1622295023876-0cdf583c41f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80"
           >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              SKILLSFUTURE MID-CAREER SUPPORT PACKAGE
+              MID-CAREER SUPPORT PACKAGE
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               This program aims to create more career transition opportunities for individuals in their 40s to 50s, and help them remain employable and be able to access employment.
@@ -37,13 +37,13 @@ export default function Carousel() {
         <div className="max-w-sm m-5">
           <Card
             imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg"
+            imgSrc="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
           >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              SKILLSFUTURE CAREER TRANSITION PROGRAMME
+              CAREER TRANSITION PROGRAM
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              The SkillsFuture Career Transition Programme (SCTP) supports mid-career individuals in acquiring industry-relevant skills to improve employability and pivot to new sectors or job roles.
+              The Career Transition Program (CTP) supports mid-career individuals in acquiring industry-relevant skills to improve employability and pivot to new sectors or job roles.
             </p>
           </Card>
         </div>
